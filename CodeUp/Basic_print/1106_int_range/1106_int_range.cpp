ee6fb71c7cc118450be39653fe4fcd1544f7f512
mskim-99 <kms99400@gmail.com>
2025-09-21 22:11:52 +0900

    1106_int_range

---
+++ CodeUp/Basic_print/1106_int_range/1106_int_range.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int min_value = -2147483648;
+  int max_value = 2147483647;
+
+  cout << min_value << " " << max_value << "\n";
+
+  return 0;
+}
