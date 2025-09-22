77d74dd4221791bfe0d730328035c2884c7a9230
mskim-99 <kms99400@gmail.com>
2025-09-22 17:18:34 +0900

    1014_change_char

---
+++ CodeUp/Basic2_print_operator/1014_change_char/1014_change_char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char value_1, value_2;
+    
+    cin >> value_1 >> value_2;
+    cout << value_2 << " " << value_1 << "\n";
+
+    return 0;
+}
