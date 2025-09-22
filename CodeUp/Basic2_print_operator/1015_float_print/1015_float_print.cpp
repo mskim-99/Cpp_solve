b59bb42cd345305d29277fd0da8c4cb30890db7a
mskim-99 <kms99400@gmail.com>
2025-09-22 17:26:58 +0900

    1015_float_print

---
+++ CodeUp/Basic2_print_operator/1015_float_print/1015_float_print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float number;
+    cin >> number;
+    cout << fixed << setprecision(2) << number << "\n";
+
+    return 0;
+}
