dc637b016f2d098f5f76f89911970650f255bc83
mskim-99 <kms99400@gmail.com>
2025-09-24 17:23:57 +0900

    1023_float_print

---
+++ CodeUp/Basic2_print_operator/1023_float_print/1023_float_print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    char dat;
+    cin >> a >> dat >> b;
+    cout << a << "\n";
+    cout << b << "\n";
+
+    return 0;
+}
