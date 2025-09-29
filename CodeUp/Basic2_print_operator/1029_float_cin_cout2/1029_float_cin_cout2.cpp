1f838d1178a258fee85dcce663a7a997df926b79
mskim-99 <kms99400@gmail.com>
2025-09-29 11:46:40 +0900

    1029_float_cin_cout2

---
+++ CodeUp/Basic2_print_operator/1029_float_cin_cout2/1029_float_cin_cout2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double number;
+    cin >> number;
+    cout << fixed << setprecision(11) << number << "\n";
+
+    return 0;
+}
