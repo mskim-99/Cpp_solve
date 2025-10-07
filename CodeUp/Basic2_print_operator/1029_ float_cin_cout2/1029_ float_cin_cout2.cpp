8abdc8aaeb7a83ebac3094213472b69394916d12
mskim-99 <kms99400@gmail.com>
2025-10-07 18:16:41 +0900

    1047_int*int

---
+++ CodeUp/Basic2_print_operator/1029_ float_cin_cout2/1029_ float_cin_cout2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float number;
+
+  cin >> number;
+  cout << number << "\n";
+
+  return 0;
+}
---
+++ CodeUp/Basic2_print_operator/1047_int*int/1047_int*int.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int value;
+  cin >> value;
+  cout << (value << 1) << "\n"; //비트 시프트 연산 2배증가
+
+  return 0;
+}
