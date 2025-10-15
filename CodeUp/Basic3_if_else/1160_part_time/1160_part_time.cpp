a7f164f8d005d30075ae3a3e64b84d7b65d8d2e9
mskim-99 <kms99400@gmail.com>
2025-10-15 20:36:24 +0900

    월,수,금,일 oh my god 출력 아니면 enjoy 출력

---
+++ CodeUp/Basic3_if_else/1160_part_time/1160_part_time.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int day;
+  cin >> day;
+
+  switch (day)
+  {
+  case 1:
+    cout << "oh my god" << "\n";
+    break;
+
+  case 3:
+    cout << "oh my god" << "\n";
+    break;
+
+  case 5:
+    cout << "oh my god" << "\n";
+    break;
+
+  case 7:
+    cout << "oh my god" << "\n";
+    break;
+  
+  default:
+    cout << "enjoy" << "\n";
+    break;
+  }
+
+  return 0;
+}
