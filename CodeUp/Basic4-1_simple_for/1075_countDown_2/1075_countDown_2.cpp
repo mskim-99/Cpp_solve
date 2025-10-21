adc6dd65e6b217f079c6244ed0dffdb2d318e94c
mskim-99 <kms99400@gmail.com>
2025-10-21 13:39:04 +0900

    입력받은 n부터 0까지 출력한다

---
+++ CodeUp/Basic4-1_simple_for/1075_countDown_2/1075_countDown_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  // 0까지 출력하기 위해 0보다 작아지면 반복 종료
+  // 입력한 값보다 1작은 값부터 0까지 출력
+  while (n > 0)
+  {
+    n--;
+    cout << n << "\n";
+  }
+
+  return 0;
+}
