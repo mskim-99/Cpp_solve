173751d81ddb0d61d4f9695b081442c9045c5a80
mskim-99 <kms99400@gmail.com>
2025-10-22 20:57:32 +0900

    1부터 입력받은 n까지 공백으로 띄워서 출력

---
+++ CodeUp/Basic4-1_simple_for/1252_1_to_n_cout/1252_1_to_n_cout.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  // 1부터 n까지 공백으로 띄워서 출력
+  for (int i = 1; i <= n; i++)
+    cout << i << " ";
+  cout << "\n";
+
+  return 0;
+}
